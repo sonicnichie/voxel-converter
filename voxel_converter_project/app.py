fe09cafed291165c927adae5f1ad3b3bf9b7d55d
sonicnichie <prasnichie@gmail.com>
2025-07-12 10:52:49 +0700

    Add files via upload

--- voxel_converter_project/app.py
+++ voxel_converter_project/app.py
@@ -10,14 +10,15 @@ st.title("🧊 3D Voxel Style Image Converter")
 
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
-block_size = st.slider("Block Size (Smaller = More Voxels)", min_value=1, max_value=10, value=4)
-z_scale = st.slider("Z Height Scale", min_value=1, max_value=50, value=10)
-brightness_threshold = st.slider("Brightness Threshold", 0, 255, 30)
+block_size = st.slider("Block Size (Smaller = More Voxels)", min_value=1, max_value=16, value=4)
+z_scale = st.slider("Z Height Scale", min_value=1, max_value=50, value=15)
+contrast_boost = st.slider("Contrast Boost (0.5 - 3.0)", min_value=0.5, max_value=3.0, value=1.5, step=0.1)
 
 if uploaded_file:
     image = Image.open(uploaded_file).convert('RGB')
     image = image.resize((64, 64))
-    img_array = np.array(image)
+    img_array = np.array(image).astype(np.float32) * contrast_boost
+    img_array = np.clip(img_array, 0, 255)
 
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111, projection='3d')
@@ -25,26 +26,27 @@ if uploaded_file:
 
     _x, _y, _z, _dx, _dy, _dz, _colors = [], [], [], [], [], [], []
 
-    for y in range(0, img_array.shape[0], block_size):
-        for x in range(0, img_array.shape[1], block_size):
-            r, g, b = img_array[y, x]
+    h, w, _ = img_array.shape
+    for y in range(0, h, block_size):
+        for x in range(0, w, block_size):
+            block = img_array[y:y+block_size, x:x+block_size]
+            r, g, b = np.mean(block[:, :, 0]), np.mean(block[:, :, 1]), np.mean(block[:, :, 2])
             brightness = 0.299*r + 0.587*g + 0.114*b
 
-            if brightness > brightness_threshold:
-                height = (brightness / 255) * z_scale
+            height = (brightness / 255.0) * z_scale
+            if height > 1:
                 _x.append(x)
-                _y.append(img_array.shape[0] - y)  # flip y for upright
+                _y.append(y)  # tidak dibalik, tampak dari atas
                 _z.append(0)
                 _dx.append(block_size)
                 _dy.append(block_size)
                 _dz.append(height)
-                _colors.append((r / 255, g / 255, b / 255))
+                _colors.append((r/255.0, g/255.0, b/255.0))
 
     ax.bar3d(_x, _y, _z, _dx, _dy, _dz, color=_colors, shade=True)
-    ax.view_init(elev=60, azim=135)
+    ax.view_init(elev=90, azim=-90)  # tampak dari atas
     st.pyplot(fig)
 
-    # Tombol download gambar
     buf = io.BytesIO()
     fig.savefig(buf, format="png")
     st.download_button("💾 Download Voxel Image as PNG", data=buf.getvalue(), file_name="voxel_output.png", mime="image/png")
